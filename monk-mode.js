76123c93fef2247707ba978502f14679a88d1d70
srpiroliro <srpiroliro21@gmail.com>
2023-01-11 11:45:25 +0100

    blocking works

--- monk-mode.js
+++ monk-mode.js
@@ -1,12 +1,35 @@
-window.addEventListener('locationchange', function () {
-    console.log('location changed!');
-    console.log(window.location.href)
-    document.body.style.border = "5px solid red";
-    console.log("helloworld")
+var YOUTUBE_SHORTS=true;
+console.log("[ MONK-MODE LOADED ]");
 
-    if(window.location.href.includes("/shorts/")){
-        console.log("got back")
-        window.history.back();
-        console.log("new url: "+window.location.href)
-    }
-});
+// made for youtube.
+window.addEventListener("yt-navigate-start",function(event){
+    if(YOUTUBE_SHORTS)
+        disable_shorts();
+})
+
+
+
+function disable_shorts() {
+    document.querySelector('[is-shorts=""]').style.display = 'none';
+    if(window.location.href.includes("shorts"))
+        // history.back()
+        block_page("detected YOUTUBE SHORTS")
+}
+
+function url_contains(text_list){
+    let current_url=window.location.href
+    text_list.forEach(element=>{
+        if(current_url.includes(element))
+            block_page("detected: "+element);
+    });
+}
+
+function content_contains(text_list){}
+
+
+function block_page(msg=""){
+    css_style="display:flex;justify-content:center;align-items:center;height:100vh"
+    document.documentElement.innerHTML="";
+    document.documentElement.innerHTML="<body style='"+css_style+"'><div><h1>BLOCKED</h1><br><p>"+msg+"</p></div></body>";
+    document.documentElement.scrollTop=0;
+}
